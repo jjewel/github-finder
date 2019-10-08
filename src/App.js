4a32794c49a7a6e9d9c74b5b237025f65447d578
Jason Jewel <jjewe2009@gmail.com>
2019-10-08 13:08:43 +1000

    User Component updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/layout/NavBar';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
@@ -11,6 +12,7 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -27,6 +29,17 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  getUser = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?
+        client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+        client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
   // Clears Users from State
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -40,7 +53,7 @@ class App extends Component {
   };
 
   render() {
-    const { alert, users, loading } = this.state;
+    const { alert, users, user, loading } = this.state;
     return (
       <Router>
         <div className='App'>
@@ -64,6 +77,18 @@ class App extends Component {
                 )}
               />
               <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
