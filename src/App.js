3a8437ba998a8b861fb529c54930077fb71c5bfb
Jason Jewel <jjewe2009@gmail.com>
2019-10-14 02:38:25 +1000

    Added page NotFound component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/layout/NavBar';
 import Alert from './components/layout/Alert';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import User from './components/users/User';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -22,6 +23,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
